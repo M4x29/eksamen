62f22fef2c4324c6564a0e592484993e72f358a4
M4x29 <maxpetzold581@gmail.com>
2024-05-28 13:07:37 +0200

    la til docker

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,6 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
+import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 export default defineConfig({
-	plugins: [sveltekit()]
+  base: "/",
+  plugins: [sveltekit()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 });
